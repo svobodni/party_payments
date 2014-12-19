0fbd59d5ec62decd26d6e20265272b4446cb57d2
Jiří Kubíček <jiri.kubicek@kraxnet.cz>
2014-12-19 23:52:30 +0100

    Deployment configuration

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,17 +1,26 @@
+module GitStrategy
+  require 'capistrano/git'
+  include Capistrano::Git::DefaultStrategy
+  def release
+    git :archive, fetch(:branch), '| tar -x -f - -C', release_path
+  end
+end
+
 # config valid only for Capistrano 3.1
 lock '3.1.0'
 
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :application, 'party_payments'
+set :repo_url, 'git@github.com:svobodni/party_payments.git'
 
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
 
 # Default deploy_to directory is /var/www/my_app
-# set :deploy_to, '/var/www/my_app'
+set :deploy_to, '/home/svobodni/finance'
 
 # Default value for :scm is :git
 # set :scm, :git
+set :git_strategy, GitStrategy
 
 # Default value for :format is :pretty
 # set :format, :pretty
@@ -24,9 +33,11 @@ set :repo_url, 'git@example.com:me/my_repo.git'
 
 # Default value for :linked_files is []
 # set :linked_files, %w{config/database.yml}
+set :linked_files, %w{config/database.yml config/configatron/production.rb}
 
 # Default value for linked_dirs is []
 # set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_dirs, %w{tmp data}
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
@@ -40,7 +51,7 @@ namespace :deploy do
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
       # Your restart mechanism here, for example:
-      # execute :touch, release_path.join('tmp/restart.txt')
+      execute :touch, release_path.join('tmp/restart.txt')
     end
   end
 
