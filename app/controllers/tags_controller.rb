6e19983eff0056bf4383782120f10837f111a052
Jiří Kubíček <jiri.kubicek@kraxnet.cz>
2014-12-21 05:21:34 +0100

    Tagy jsou společné pro celou aplikaci

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -3,11 +3,7 @@ class TagsController < ApplicationController
   # GET /tags
   # GET /tags.json
   def index
-    if @organization
-      @tags = @organization.owned_tags
-    else
-      @tags = ActsAsTaggableOn::Tag.most_used
-    end
+    @tags = ActsAsTaggableOn::Tag.most_used
   end
 
 end
--- app/models/accounting.rb
+++ app/models/accounting.rb
@@ -3,11 +3,4 @@ class Accounting < ActiveRecord::Base
   belongs_to :budget_category
 
   acts_as_taggable
-  before_save :set_tag_owner
-  def set_tag_owner
-    # Set the owner of some tags based on the current tag_list
-    set_owner_tag_list_on(self.budget_category.organization, :tags, self.tag_list)
-    # Clear the list so we don't get duplicate taggings
-    self.tag_list = nil
-  end
 end
