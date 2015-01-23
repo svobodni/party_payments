bfb85ba7134c53ad9f5e484e3531ee53f49ac8ab
Jiří Kubíček <jiri.kubicek@kraxnet.cz>
2015-01-23 20:51:36 +0100

    log je nyní shared mezi releasema

--- config/deploy.rb
+++ config/deploy.rb
@@ -36,7 +36,7 @@ set :linked_files, %w{config/database.yml config/configatron/production.rb}
 
 # Default value for linked_dirs is []
 # set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
-set :linked_dirs, %w{tmp data}
+set :linked_dirs, %w{tmp log data}
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
