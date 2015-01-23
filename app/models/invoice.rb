517b611e5010afde5b7fc5f21eecef51cfdaed09
Jiří Kubíček <jiri.kubicek@kraxnet.cz>
2015-01-23 19:12:12 +0100

    Attachmenty jsou v /data sdíleném skrz releasy

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -4,7 +4,7 @@ class Invoice < ActiveRecord::Base
   has_many :accountings, as: :accountable
   has_many :payments, as: :payable
 
-  has_attached_file :document #, path: ":rails_root/data/invoices/:id.pdf", url: "/people/:person_id/signed_application.pdf"
+  has_attached_file :document, path: ":rails_root/data/invoices/:id.pdf"
   validates_attachment :document, content_type: { content_type: "application/pdf" }
 
   def accounting_remainder
