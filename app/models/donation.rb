117dbc659ab610f1311a5ef1429ab4b499bb0154
Jiří Kubíček <jiri.kubicek@kraxnet.cz>
2015-06-29 01:16:13 +0200

    Automatické přiřazování darů a členských příspěvků do letošního republikového rozpočtu

--- app/models/donation.rb
+++ app/models/donation.rb
@@ -3,6 +3,7 @@ class Donation < ActiveRecord::Base
   belongs_to :organization
   has_many :accountings, as: :accountable
   has_many :payments, as: :payable
+  before_create :set_accounting
 
   attr_accessor :bank_payment_id, :budget_category_id
 
@@ -24,6 +25,12 @@ class Donation < ActiveRecord::Base
 
   def number
     "#{received_on.year}#{organization.id.to_s.rjust(2,'0')}#{id.to_s.rjust(3,'0')}"
+  end
+
+  def set_accounting
+    if received_on > Date.parse("2014-12-31")
+      self.accountings.build(budget_category_id: 11, amount: amount) if organization_id==100
+    end if accountings.empty?
   end
 
 end
--- app/models/membership_fee.rb
+++ app/models/membership_fee.rb
@@ -3,6 +3,7 @@ class MembershipFee < ActiveRecord::Base
   belongs_to :region, class: Organization
   has_many :accountings, as: :accountable
   has_many :payments, as: :payable
+  before_create :set_accounting
 
   attr_accessor :bank_payment_id, :budget_category_id
 
@@ -12,6 +13,12 @@ class MembershipFee < ActiveRecord::Base
 
   def self.grouped_by_quarter_and_region
     all.group_by {|mf| "#{mf.received_on.year}, #{((mf.received_on.month - 1) / 3) + 1}, #{mf.region_id}"}
+  end
+
+  def set_accounting
+    if received_on > Date.parse("2014-12-31")
+      self.accountings.build(budget_category_id: 8, amount: amount)
+    end if accountings.empty?
   end
 
 end
