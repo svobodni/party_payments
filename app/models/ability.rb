ca478031a49b537dee4407389cf79bb8c29e7a5b
Jiri Kubicek <jiri.kubicek@kraxnet.cz>
2016-12-07 00:01:57 +0100

    Ability fix

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,7 +5,7 @@ class Ability
   def initialize(user)
 
     orgs={1 => 100, 2 => 100, 3 => 100, 4 => 100, 5 => 100, 6 => 1, 7 => 2, 8 => 3, 9 => 4, 10 => 5, 11 => 6, 12 => 7, 13 => 8, 14 => 9, 15 => 10, 16 => 11, 17 => 12, 18 => 13, 19 => 14}
-    can :read, :all
+    # can :read, :all
     user = user['info']['person']
     user['roles'].each do |role|
       if (role['name'] == "President" || role['name'] == "Vicepresident") && role['organization']['name']=="Krajské předsednictvo"
@@ -14,11 +14,18 @@ class Ability
       elsif role['organization'].try(:'id')=="1"
         # Členové republikového předsednictva
         can [:manage], [Donation,Invoice,BudgetCategory], organization_id: 100
+      elsif role['organization'].try(:'id')=="2"
+        # Kontrolní komise
+        can :read, :all
       end
     end
 
     if [42, 342, 344, 2804, 3860].member?(user['id'])
       can :manage, :all
+    end
+
+    if user['id']==2522
+      can :read, :all
     end
 
   end
