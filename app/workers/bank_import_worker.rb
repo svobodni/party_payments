55f2ea596897197dc40ba0de1fdf45bc8d023d7f
Jiri Kubicek <jiri.kubicek@kraxnet.cz>
2017-01-02 03:19:40 +0100

    Vypnuto stahovani starych bank a gopay

--- app/workers/bank_import_worker.rb
+++ app/workers/bank_import_worker.rb
@@ -2,9 +2,9 @@ class BankImportWorker
   include Sidekiq::Worker
 
   def perform
-    BankPayment.import
-    BankPayment.where("paid_on>'2015-12-31' and organization_id is not null").each{|bp| bp.pair}
-    GopayPayment.import
+#    BankPayment.import
+#    BankPayment.where("paid_on>'2015-12-31' and organization_id is not null").each{|bp| bp.pair}
+#    GopayPayment.import
     BankAccount.first.import
   end
 end
