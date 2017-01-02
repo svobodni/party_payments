becdc4ab369d3edb56c59ba8b5082cf6c04c1746
Jiri Kubicek <jiri.kubicek@kraxnet.cz>
2017-01-02 04:39:16 +0100

    Stav prostředků republikové kanceláře

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -10,5 +10,12 @@ class OrganizationsController < ApplicationController
   # GET /organizations/1.json
   def show
     @organization = Organization.find(params[:id])
+    @bank_balance = if @organization.id==100
+      BankAccount.sum(:balance)-Organization.sum(:bank_balance)
+    else
+      @organization.bank_balance
+    end
+    @cash_balance = @organization.cash_balance
+    @balance = @bank_balance||0 + @cash_balance||0
   end
 end
