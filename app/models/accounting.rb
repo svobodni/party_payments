3bd55a0d318093fe3a3df823379e59952a2b4c18
Jiri Kubicek <jiri.kubicek@kraxnet.cz>
2017-01-04 02:14:09 +0100

    Lepší výpis detailů rozpočtové kapitoly při vazbě na platbu

--- app/models/accounting.rb
+++ app/models/accounting.rb
@@ -1,6 +1,7 @@
 class Accounting < ActiveRecord::Base
   belongs_to :accountable, polymorphic: true
   belongs_to :budget_category
+  belongs_to :payment, polymorphic: true
 
   acts_as_taggable
 end
