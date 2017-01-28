a3230a3e4011c9c0c067235544f214fddded731f
Jiri Kubicek <jiri.kubicek@kraxnet.cz>
2017-01-28 14:57:03 +0100

    Revert "Automaticke parovani clenskych prispevku"
    This reverts commit 40b41c9fc36747b505fa8b2f61fc9a18c9a7b235.

--- app/workers/bank_import_worker.rb
+++ app/workers/bank_import_worker.rb
@@ -5,7 +5,6 @@ class BankImportWorker
 #    BankPayment.import
 #    BankPayment.where("paid_on>'2015-12-31' and organization_id is not null").each{|bp| bp.pair}
 #    GopayPayment.import
-    BankPayment.where(our_account_number:2100382818).where("id>12280").each{|bp| bp.pair}
     BankAccount.all.each{|bank_account| bank_account.import}
   end
 end
