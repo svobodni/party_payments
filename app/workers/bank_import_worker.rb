ef0ad2295d6ac5272d054b27ad7a1b29ae3e1f63
Jiri Kubicek <jiri.kubicek@kraxnet.cz>
2017-04-03 16:34:50 +0200

    Parovani odchozich plateb

--- app/workers/bank_import_worker.rb
+++ app/workers/bank_import_worker.rb
@@ -3,10 +3,10 @@ class BankImportWorker
 
   def perform
 #    BankPayment.import
-#    BankPayment.where("paid_on>'2015-12-31' and organization_id is not null").each{|bp| bp.pair}
 #    GopayPayment.import
     last_bank_payment = BankPayment.last
     BankAccount.all.each{|bank_account| bank_account.import}
     BankPayment.where(our_account_number:2100382818).where("id>#{last_bank_payment.id}").each{|bp| bp.pair}
+    BankPayment.where("paid_on>'2016-12-31' and amount<0").includes(:payments).select{|bp| bp.returned_payment.blank?}.select{|bp| bp.payments.blank?}.each{|bp| bp.pair}
   end
 end
