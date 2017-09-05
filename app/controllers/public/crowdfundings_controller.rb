48fb908182539b885c513c1171715453ddf353ed
Jiri Kubicek <jiri.kubicek@kraxnet.cz>
2017-09-05 23:26:19 +0200

    [CROWDFUNDING] dynamické FB meta tagy

--- app/controllers/public/crowdfundings_controller.rb
+++ app/controllers/public/crowdfundings_controller.rb
@@ -2,13 +2,24 @@ class Public::CrowdfundingsController < ApplicationController
 
   layout 'crowdfunding'
   skip_before_filter :authenticate_person!
+  def default_url_options
+    {}
+  end
 
   def index
     @crowdfunding = Crowdfunding.all
+    @og_title="Přispějte na kampaň Svobodných!"
+    @og_url=public_crowdfundings_url
+    @og_description="Namixujte kampaň Svobodných podle svých představ a pomozte nám uspět ve volbách. Můžeme být svobodnou zemí."
+    @og_image=ActionController::Base.helpers.image_url "crowdfunding/9907.jpg"
   end
 
   def show
     @crowdfunding = Crowdfunding.find(params[:id])
+    @og_title="Přispějte na kampaň Svobodných! #{@crowdfunding.title}"
+    @og_url=public_crowdfunding_url(params[:id])
+    @og_description=@crowdfunding.perex
+    @og_image=ActionController::Base.helpers.image_url "crowdfunding/#{@crowdfunding.image_url}"
     @donation = Donation.new(vs_prefix:@crowdfunding.vs_prefix, amount: @crowdfunding.price)
   end
 
