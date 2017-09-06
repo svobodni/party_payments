816c6a5487554b66902bcdc7eac1664cc2a5eb2b
Pavel Mises <id@dqd.cz>
2017-09-06 13:18:29 +0200

    Doladění titulku a obrázku pro Facebook

--- app/controllers/public/crowdfundings_controller.rb
+++ app/controllers/public/crowdfundings_controller.rb
@@ -8,15 +8,15 @@ class Public::CrowdfundingsController < ApplicationController
 
   def index
     @crowdfunding = Crowdfunding.all
-    @og_title="Přispějte na kampaň Svobodných!"
+    @og_title="Zviditelněte Svobodné"
     @og_url=public_crowdfundings_url
-    @og_description="Namixujte kampaň Svobodných podle svých představ a pomozte nám uspět ve volbách. Můžeme být svobodnou zemí."
-    @og_image=ActionController::Base.helpers.image_url "crowdfunding/9907.jpg"
+    @og_description="Namixujte kampaň Svobodných podle svých představ, aby Svobodní uspěli ve volbách. Můžeme být svobodnou zemí."
+    @og_image=ActionController::Base.helpers.image_url "facebook-crowdfunding.jpg"
   end
 
   def show
     @crowdfunding = Crowdfunding.find(params[:id])
-    @og_title="Přispějte na kampaň Svobodných! #{@crowdfunding.title}"
+    @og_title="Zviditelněte Svobodné: #{@crowdfunding.title}"
     @og_url=public_crowdfunding_url(params[:id])
     @og_description=@crowdfunding.perex
     @og_image=ActionController::Base.helpers.image_url "crowdfunding/#{@crowdfunding.image_url}"
