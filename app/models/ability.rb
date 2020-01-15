d82709fa38c81dd4a47fc498f9c3dca5ba1d2de0
Jiri Kubicek <jiri.kubicek@kraxnet.cz>
2020-01-15 11:04:18 +0100

    Opravneni

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,7 +3,7 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    if [42, 342, 344, 2804, 3860, 7873].member?(user['id'])
+    if [342, 1728, 4039, 7873].member?(user['id'])
       can :manage, :all
     end
 
